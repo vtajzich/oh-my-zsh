94dc7fb141635379e56a1aea36fb18dd70cd69ff
Vitezslav Tajzich <v.tajzich@gmail.com>
2018-07-03 08:42:43 +0200

    changed to be AWS EC2 compatible

--- tools/install.sh
+++ tools/install.sh
@@ -79,18 +79,20 @@ main() {
   mv -f ~/.zshrc-omztemp ~/.zshrc
 
   # If this user's login shell is not already "zsh", attempt to switch.
-  TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
-  if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  #TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
+  #if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
     # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  #  if hash chsh >/dev/null 2>&1; then
+  #    printf "${BLUE}Time to change your default shell to zsh!${NORMAL}\n"
+  #    chsh -s $(grep /zsh$ /etc/shells | tail -1)
     # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
-    fi
-  fi
+  #  else
+  #    printf "I can't change your shell automatically because this system does not have chsh.\n"
+  #    printf "${BLUE}Please manually change your default shell to zsh!${NORMAL}\n"
+  #  fi
+  #fi
+
+  sudo chsh -s /bin/zsh ec2-user
 
   printf "${GREEN}"
   echo '         __                                     __   '
@@ -108,7 +110,7 @@ main() {
   echo 'p.p.s. Get stickers and t-shirts at https://shop.planetargon.com.'
   echo ''
   printf "${NORMAL}"
-  env zsh -l
+  #env zsh -l
 }
 
 main
